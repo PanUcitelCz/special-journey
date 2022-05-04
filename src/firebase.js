4c0897bef5e56a34611ffb3913e0d15beffcf9af
PanUcitelCz <petr.b257@seznam.cz>
2022-05-04 21:19:24 +0200

    comit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB3lM0I9fAHpID3L4OXKEOe8O2WjWrSKBM",
+  authDomain: "special-journey.firebaseapp.com",
+  projectId: "special-journey",
+  storageBucket: "special-journey.appspot.com",
+  messagingSenderId: "737140243058",
+  appId: "1:737140243058:web:6a4fc2472b3cc20e81a0eb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,6 +6,23 @@
 	}
 </script>
 
-<button on:click={incrementCount}>
-	Clicked {count} {count === 1 ? 'time' : 'times'}
-</button>
+<div class="wraper">
+    <button on:click={incrementCount}>
+        Clicked {count} {count === 1 ? 'time' : 'times'}
+    </button>
+</div>
+
+<style lang="scss">
+    .wraper{
+        margin: 0 auto;
+
+        button {
+            color: rgb(185, 44, 19);
+            background: blue;
+            border-radius: 13px;
+            width: 50%;
+            height: 50px;    
+        }   
+    }
+    
+</style>
